85df3baf9385f36a715f7bff9c32046cc3d7edb7
Aravinda1738 <b.aravinda.official@gmail.com>
2023-05-27 22:16:41 +0530

    update 2 animation work

--- fps2/Assets/Gun.cs
+++ fps2/Assets/Gun.cs
@@ -9,22 +9,28 @@ public class Gun : MonoBehaviour
     public Camera cam;
     public LayerMask mask=new LayerMask();
     public GameObject aimPoint;
-
+   
     // Update is called once per frame
     void Update()
     {
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
 
+      
 
-        
-            
+        if (Input.GetMouseButtonUp(1))
+        {
+            aimPoint.SetActive(false);
+        }
+
+
             if (Physics.Raycast(ray, out RaycastHit hit, range, mask))
             {
-                Debug.Log(hit.transform.position + "  " + hit.collider.name);
+                Debug.Log(hit.transform.position + " " + hit.collider.name);
 
                 aimPoint.transform.position = hit.point;
             }
+        
        
 
         
--- fps2/Assets/scripts/Movement.cs
+++ fps2/Assets/scripts/Movement.cs
@@ -14,18 +14,22 @@ public class Movement : MonoBehaviour
    public Transform t,aimT;
     public AimCam ac;
     public Rig rig;
-    public bool aim;
-  //  public CinemachineBrain cineBrain;
+    public bool aimEnd;
+    public Animator ani;
+    float aniMovSpeed=0;
+ [SerializeField] [Range(0f, 30f)] float lerpTime;
+    //  public CinemachineBrain cineBrain;
 
     /* public float damage = 10f;
       float range = 999f;
      public Camera aimCamera;
 
      public LayerMask mask = new LayerMask();*/
-  
+
     // Start is called before the first frame update
     void Start()
     {
+        aimEnd = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -42,52 +46,84 @@ public class Movement : MonoBehaviour
         Vector3 move = new Vector3(x,0f,z).normalized;
 
 
-       /* Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
-        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        /* Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
 
-      
-        
 
-        if (Physics.Raycast(ray, out RaycastHit hit, range, mask))
+
+
+         if (Physics.Raycast(ray, out RaycastHit hit, range, mask))
+         {
+             Debug.Log(hit.transform.position + "  " + hit.collider.name);
+
+             aimT.position = hit.point;
+         }
+ */
+        if (move.magnitude >= 0.1f) //movement
         {
-            Debug.Log(hit.transform.position + "  " + hit.collider.name);
+          
 
-            aimT.position = hit.point;
+            float ang = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            float finalAng = Mathf.SmoothDampAngle(transform.eulerAngles.y, ang, ref tempAng, angSmooth);
+            transform.rotation = Quaternion.Euler(0f, finalAng, 0f);
+            movDir = Quaternion.Euler(0f, finalAng, 0f) * Vector3.forward;
+            cc.Move(movDir.normalized * speed * Time.deltaTime);
+            ani.SetBool("run", true);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                aniMovSpeed = Mathf.Lerp(aniMovSpeed,2f,lerpTime*Time.deltaTime);
+            }
+            else
+            {
+              aniMovSpeed = Mathf.Lerp(aniMovSpeed, 1f, lerpTime * Time.deltaTime);
+                    
+            }
+            ani.SetFloat("mov",aniMovSpeed);
         }
-*/
+        else
+        {
+            ani.SetBool("run", false);
+                
+        }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)) //escape
         {
            Cursor.lockState = CursorLockMode.None;
         }
         
-        if (Input.GetKey(KeyCode.Mouse1))
+        if (Input.GetKey(KeyCode.Mouse1))//aim
         {
             aimView.Priority = 14;
 
             ac.aim();
             rig.weight = 1f;
-            aim = true;
+            ani.SetBool("aim", true);
             mark.SetActive(true);
         }
         else
         {
-            aim = false;
+           
             aimView.Priority = 10;
             rig.weight = 0f;
             mark.SetActive(false);
-            if (move.magnitude >= 0.1f)
-            {
-                float ang = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
-                float finalAng = Mathf.SmoothDampAngle(transform.eulerAngles.y, ang, ref tempAng, angSmooth);
-                movDir = Quaternion.Euler(0, finalAng, 0) * Vector3.forward;
-                transform.rotation = Quaternion.Euler(0, finalAng, 0);
-                cc.Move(movDir * speed * Time.deltaTime);
-            }
+            
         }
-        
-       
 
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            aimEnd = false;
+            ani.SetBool("aim", false);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ani.SetTrigger("fire");
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            ani.ResetTrigger("fire");
+        }
 
     }
 }
